172a07db67d72ba51c8c182955a84a14e5e4a84e
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-07-07 00:14:31 +0100

    added attack1 animation
    needs cleaning up

--- src/index.js
+++ src/index.js
@@ -6,17 +6,65 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.atlas("zombie", "assets/Sprites/Attack1.png");
+    this.load.image(
+      "attack1001",
+      "./src/assets/Sprites/attack1anim/Attack1001.png"
+    );
+    this.load.image(
+      "attack1002",
+      "./src/assets/Sprites/attack1anim/Attack1002.png"
+    );
+    this.load.image(
+      "attack1003",
+      "./src/assets/Sprites/attack1anim/Attack1003.png"
+    );
+    this.load.image(
+      "attack1004",
+      "./src/assets/Sprites/attack1anim/Attack1004.png"
+    );
+    this.load.image(
+      "attack1005",
+      "./src/assets/Sprites/attack1anim/Attack1005.png"
+    );
+    this.load.image(
+      "attack1006",
+      "./src/assets/Sprites/attack1anim/Attack1006.png"
+    );
+    this.load.image(
+      "attack1007",
+      "./src/assets/Sprites/attack1anim/Attack1007.png"
+    );
+    this.load.image(
+      "attack1008",
+      "./src/assets/Sprites/attack1anim/Attack1008.png"
+    );
   }
 
   create() {
     this.anims.create({
-      key: "walk",
-      frames: "zombie",
+      key: "attack1",
+      frames: [
+        {
+          key: "attack1001",
+          key: "attack1002",
+          key: "attack1003",
+          key: "attack1004",
+          key: "attack1005",
+          key: "attack1006",
+          key: "attack1007",
+          key: "attack1008",
+        },
+      ],
       frameRate: 12,
-      repeat: -1,
+      repeat: 1,
     });
+
+    this.animatedSpace = this.add
+      .sprite(400, 300, "attack1001")
+      .play("attack1");
   }
+
+  update() {}
 }
 
 const config = {
